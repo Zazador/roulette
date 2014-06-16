46381d764436b53b3b31712dc31f4ac4f3fcca52
Zazador <zazador@gmail.com>
2014-06-16 12:26:57 -0500

    Added Favorites fragment and changed for only one login screen.

---
+++ app/src/main/java/com/example/roulette/app/FavoriteFragment.java
@@ -0,0 +1,23 @@
+package com.example.roulette.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zach on 6/16/14.
+ */
+public class FavoriteFragment extends Fragment {
+    public FavoriteFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/roulette/app/HomeFragment.java
+++ app/src/main/java/com/example/roulette/app/HomeFragment.java
@@ -29,112 +29,14 @@ import com.parse.SignUpCallback;
  * Created by zach on 6/11/14.
  */
 public class HomeFragment extends Fragment {
-    // Declare Variables
-    Button loginbutton;
-    Button signup;
-    String usernametxt;
-    String passwordtxt;
-    EditText password;
-    EditText username;
+        public HomeFragment() {}
 
-    public HomeFragment() {}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
 
-    /**
-     * THIS NEEDS TO BE IMPLEMENTED BEFORE YOU CAN USE getActivity()
-     */
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        DBHelper = new DatabaseHelper(activity);
-//    }
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        // Locate EditTexts in fragment_home.xml
-        username = (EditText) view.findViewById(R.id.username);
-        password = (EditText) view.findViewById(R.id.password);
-
-        // Locate Buttons in fragment_home.xml
-        loginbutton = (Button) view.findViewById(R.id.login);
-        signup = (Button) view.findViewById(R.id.signup);
-
-        // Login Button Click Listener
-        loginbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Retrieve the text entered from the EditText
-                usernametxt = username.getText().toString();
-                passwordtxt = password.getText().toString();
-
-                // Send data to Parse.com for verification
-                ParseUser.logInInBackground(usernametxt, passwordtxt, new LogInCallback() {
-                    @Override
-                    public void done(ParseUser user, ParseException e) {
-                        if (user != null) {
-                            // If user exists and authenticated, send user to Find Restaurant Fragment
-
-                            // Hides keyboard before switching fragments
-                            final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                            imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
-
-                            // Create new fragment and transaction
-                            FindRestaurantFragment newFragment = new FindRestaurantFragment();
-                            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-                            // Replace whatever is in the fragment_container view with this fragment,
-                            // and add the transaction to the back stack
-                            transaction.replace(R.id.frame_container, newFragment);
-                            transaction.addToBackStack(null);
-
-                            // Commit the transaction
-                            transaction.commit();
-
-                            Toast.makeText(getActivity(), "Successfully logged in!", Toast.LENGTH_LONG).show();
-                        } else {
-                            // If user does not exist
-                            Toast.makeText(getActivity(), "No such user exists. Please register.", Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-                });
-            }
-        });
-
-        //Sign up Button Click Listener
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Retrieve the ext entered from EditText
-                usernametxt = username.getText().toString();
-                passwordtxt = password.getText().toString();
-
-                // Force user to fill up the form
-                if (usernametxt.equals("") || passwordtxt.equals("")) {
-                    Toast.makeText(getActivity(), "Please fully complete the forms.", Toast.LENGTH_LONG).show();
-                } else {
-                    // Save new user data into Parse.com Data Storage
-                    ParseUser user = new ParseUser();
-                    user.setUsername(usernametxt);
-                    user.setPassword(passwordtxt);
-                    user.signUpInBackground(new SignUpCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            if (e == null) {
-                                // Show a simple Toast message upon successful registration
-                                Toast.makeText(getActivity(), "Sucessfully signed up! Please login.", Toast.LENGTH_LONG).show();
-                            } else {
-                                Toast.makeText(getActivity(), "Sign Up Error.", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-                }
-            }
-        });
-
-        return view;
+            return rootView;
+        }
     }
-
-
-}
