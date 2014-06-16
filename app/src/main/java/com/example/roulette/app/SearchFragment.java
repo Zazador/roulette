f635d9dab56a44d88cea7246cf52e0480b9e675c
Zazador <zazador@gmail.com>
2014-06-16 16:41:15 -0500

    SearchFragment will search auto based on set coords
    Fails due to being on main thread. need to async

--- app/src/main/java/com/example/roulette/app/SearchFragment.java
+++ app/src/main/java/com/example/roulette/app/SearchFragment.java
@@ -1,29 +1,19 @@
 package com.example.roulette.app;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.parse.LogInCallback;
-import com.parse.ParseAnonymousUtils;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+import org.w3c.dom.Text;
 
 /**
  * Created by zach on 6/11/14.
@@ -35,7 +25,28 @@ public class SearchFragment extends Fragment {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+            // Execute a signed call to the Yelp service
+            OAuthService service = new ServiceBuilder()
+                    .provider(YelpV2API.class)
+                    .apiKey("M8HBn3S_-bKLXLIRemDf9w")
+                    .apiSecret("iynjTDafI6Tth7fPtiz56vJLd70")
+                    .build();
+            Token accessToken = new Token("z6jDMmgQEE60NQMWl70oVGd6x--Ypv5Y", "bgeFFbkd5GXttsHzYtIsehYUHFE");
+
+            // We want to perform a search.
+            OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.yelp.com/v2/search");
+            // Based on a GPS coordinate latitude/longitude
+            request.addQuerystringParameter("ll", "30.267153, -97.743061");
+            // Looking for any restaurants
+            request.addQuerystringParameter("category", "restaurants");
+            service.signRequest(accessToken, request);
+            Response response = request.send();
+            String rawData = response.getBody();
+
+            TextView tv = (TextView) rootView.findViewById(R.id.txtLabel);
+            tv.setText(rawData);
 
             return rootView;
         }
